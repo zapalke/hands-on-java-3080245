cba64df61cf9953c43670638d9e49032283cf051
zapalke <103122796+zapalke@users.noreply.github.com>
2024-05-14 10:51:37 +0000

    2_e3

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.security.auth.login.AccountExpiredException;
+
 public class DataSource {
 
   public static Connection connect() {
@@ -44,8 +46,30 @@ public class DataSource {
     return customer;
   }
 
+  public static Account getAccount(int id) {
+    String sql = "select * from accounts where id = ?";
+    Account account = null;
+
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+      statement.setInt(1, id);
+      try (ResultSet resultSet = statement.executeQuery()) {
+        account = new Account(
+            resultSet.getInt("id"),
+            resultSet.getString("type"),
+            resultSet.getDouble("balance"));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return account;
+  }
+
   public static void main(String[] args) {
     Customer customer = getCustomer("twest8o@friendfeed.com");
     System.out.println(customer.getName());
+
+    Account account = getAccount(10385);
+    System.out.println(account.getBalance());
   }
 }
