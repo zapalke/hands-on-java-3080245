fade8d8c00b41b5e101188023186927c3201de51
zapalke <103122796+zapalke@users.noreply.github.com>
2024-05-14 10:44:43 +0000

    2_e02

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,6 +2,8 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -19,7 +21,31 @@ public class DataSource {
     return connection;
   }
 
+  public static Customer getCustomer(String username) {
+    String sql = "select * from customers where username = ?";
+    Customer customer = null;
+    try (Connection connection = connect();
+        PreparedStatement statement = connection.prepareStatement(sql)) {
+
+      statement.setString(1, username);
+      try (ResultSet resultSet = statement.executeQuery()) {
+        customer = new Customer(
+            resultSet.getInt("id"),
+            resultSet.getString("name"),
+            resultSet.getString("username"),
+            resultSet.getString("password"),
+            resultSet.getInt("account_id"));
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    return customer;
+  }
+
   public static void main(String[] args) {
-    connect();
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+    System.out.println(customer.getName());
   }
 }
